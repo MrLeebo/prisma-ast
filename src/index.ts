bc5a31ecd678799163d2fa1cbe2a2a60d0531d09
Jeremy Liberman <jeremy.liberman@zelis.com>
2023-07-19 09:14:43 -0500

    Include CstNodeLocation in exported types

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './getSchema';
 export * from './printSchema';
 export * from './PrismaSchemaBuilder';
 export type { PrismaAstConfig } from './getConfig';
+export type { CstNodeLocation } from 'chevrotain';
